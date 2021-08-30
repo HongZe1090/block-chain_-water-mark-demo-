9198a2c3a287cf49fb170e73899cfbe862e208db
1090414202 <1090414202@qq.com>
2021-08-30 10:16:30 +0800

    Http后端Springboot

--- asset-app/src/main/java/org/fisco/bcos/ClientReceipt/util/Test.java
+++
@@ -1,6 +0,0 @@
-package org.fisco.bcos.ClientReceipt.util;
-
-public class Test {
-    public static void main(String[] args) {
-    }
-}
--- asset-app/src/main/java/org/fisco/bcos/transhash/trans/Hello.java
+++
@@ -1,7 +0,0 @@
-package org.fisco.bcos.transhash.trans;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("123123");
-    }
-}
